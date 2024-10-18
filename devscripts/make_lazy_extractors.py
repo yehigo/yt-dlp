59c042741805d584de9324720b087911fdce1993
Pintu Lal Meena <pintulalmee@gmail.com>
2024-10-18 15:40:00 +0530

    added batching for lazy load extractor class by moving modules to helper classes to keep the file size in limit

--- devscripts/make_lazy_extractors.py
+++ devscripts/make_lazy_extractors.py
@@ -31,12 +31,18 @@ class {name}({bases}):
 '''
 MODULE_TEMPLATE = read_file('devscripts/lazy_load_template.py')
 
+_helper_number =0
+_helper_number_of_module = 100
 
 def main():
     os.environ['YTDLP_NO_PLUGINS'] = 'true'
     os.environ['YTDLP_NO_LAZY_EXTRACTORS'] = 'true'
 
     lazy_extractors_filename = get_filename_args(default_outfile='yt_dlp/extractor/lazy_extractors.py')
+    helperbase = get_filename_args(default_outfile='yt_dlp/extractors_helper/helper0.py')
+    if not os.path.exists('yt_dlp/extractors_helper'):
+        os.makedirs('yt_dlp/extractors_helper')
+
 
     from yt_dlp.extractor.extractors import _ALL_CLASSES
     from yt_dlp.extractor.common import InfoExtractor, SearchInfoExtractor
@@ -46,11 +52,13 @@ def main():
         MODULE_TEMPLATE,
         '    _module = None',
         *extra_ie_code(DummyInfoExtractor),
-        '\nclass LazyLoadSearchExtractor(LazyLoadExtractor):\n    pass\n',
-        *build_ies(_ALL_CLASSES, (InfoExtractor, SearchInfoExtractor), DummyInfoExtractor),
+        '\nclass LazyLoadSearchExtractor(LazyLoadExtractor):\n    pass\n' 
     ))
 
-    write_file(lazy_extractors_filename, f'{module_src}\n')
+    write_file(helperbase, f'{module_src}\n')
+    lazy_extractors_module = '\n'.join((build_ies(_ALL_CLASSES, (InfoExtractor, SearchInfoExtractor), DummyInfoExtractor)))
+    write_file(lazy_extractors_filename, f'\n{lazy_extractors_module}\n')
+    
 
 
 def extra_ie_code(ie, base=None):
@@ -66,13 +74,83 @@ def extra_ie_code(ie, base=None):
             yield getsource(f)
 
 
+def gethelper_files(fileName):
+    file = get_filename_args(default_outfile='yt_dlp/extractors_helper/'+(fileName))
+    return file
+def get_extractor_helper_numer():
+    global _helper_number
+    _helper_number +=1
+    return _helper_number
+def peak_next_extractor_helper_numer():
+    global _helper_number
+    return _helper_number+1
+
+def get_base(ie):
+    bases = ', '.join({
+        'InfoExtractor': 'LazyLoadExtractor',
+        'SearchInfoExtractor': 'LazyLoadSearchExtractor',
+    }.get(base.__name__, base.__name__) for base in ie.__bases__)
+    return bases
+
+module_map = {"LazyLoadMetaClass": 0, "LazyLoadExtractor": 0, "LazyLoadSearchExtractor": 0,}
+
+def write_helper_file(helper_import_name, modules, helper_self_import):
+    next_peak = peak_next_extractor_helper_numer()
+    global module_map
+    imports_to_add = ""
+    imports_from_file ={}
+    for im in helper_self_import:
+        import_from = module_map.get(im)
+        if import_from == None:
+            print("import not found for:" + im )
+            #print(module_map)
+        else:
+            imfp = imports_from_file.get(import_from)
+            if imfp == None:
+                imfp = [im]
+                imports_from_file[import_from] = imfp
+            else:
+                imfp.append(im)
+
+    for imfp in imports_from_file:
+        if imfp != next_peak:
+            imports_to_add += "from helper"+ str(imfp)+ " import " + (", ".join(imports_from_file.get(imfp)))+ "\n"
+            
+            
+    
+    helper_number = get_extractor_helper_numer()
+    fineName = "helper"+(str(helper_number))
+    file =  gethelper_files(fineName +".py")
+    write_file(file, f'{imports_to_add + modules}\n')
+    return "from ..extractors_helper.helper"+(str(helper_number))+(" import ")+(", ".join(helper_import_name))
+
+
+
 def build_ies(ies, bases, attr_base):
+    global module_map
     names = []
+    modules_ = "\n"
+    helper_import_name = []
+    helper_self_import =  set()
     for ie in sort_ies(ies, bases):
-        yield build_lazy_ie(ie, ie.__name__, attr_base)
+        tmp =  build_lazy_ie(ie, ie.__name__, attr_base)
+        modules_ = modules_+tmp+'\n'
+        module_map[ie.__name__] = peak_next_extractor_helper_numer()
+        for x in get_base(ie).split(","):
+            helper_self_import.add(x.strip())
         if ie in ies:
             names.append(ie.__name__)
-
+            helper_import_name.append(ie.__name__)
+        if len(helper_import_name) > _helper_number_of_module :
+           yield write_helper_file(helper_import_name, modules_, helper_self_import)
+           helper_import_name = []
+           modules_ = "\n"
+           helper_self_import.clear()
+        
+    yield write_helper_file(helper_import_name, modules_, helper_self_import)
+    helper_import_name = []
+    modules_ = "\n"
+    helper_self_import.clear()
     yield f'\n_ALL_CLASSES = [{", ".join(names)}]'
 
 
@@ -100,10 +178,7 @@ def sort_ies(ies, ignored_bases):
 
 
 def build_lazy_ie(ie, name, attr_base):
-    bases = ', '.join({
-        'InfoExtractor': 'LazyLoadExtractor',
-        'SearchInfoExtractor': 'LazyLoadSearchExtractor',
-    }.get(base.__name__, base.__name__) for base in ie.__bases__)
+    bases = get_base(ie)
 
     s = IE_TEMPLATE.format(name=name, module=ie.__module__, bases=bases)
     return s + '\n'.join(extra_ie_code(ie, attr_base))
